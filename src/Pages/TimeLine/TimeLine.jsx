27c8aa80f3301b4719b03d241b2c0f6beab568dd
muradvc <muradvcv@gmail.com>
2026-04-16 10:43:46 +0600

    collected own contex in timeline

---
+++ src/Pages/TimeLine/TimeLine.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { timeLineContext } from '../../Context/TimeLineProvider';
+
+const TimeLine = () => {
+
+  const { timeLine } = useContext(timeLineContext);
+
+  
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default TimeLine;
